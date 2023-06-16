230aa3cdeb63861bcb143a30bd5c9874dc06c0ed
l0ksh <lokesh904@gmail.com>
2023-06-16 23:24:26 +0530

    Updated Login Page

--- src/main/java/com/example/radical/login.java
+++ src/main/java/com/example/radical/login.java
@@ -1,4 +1,13 @@
 package com.example.radical;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class login {
+    @GetMapping("/login")
+    public String getData(){
+        return "This is Login Page";
+    }
+
 }
